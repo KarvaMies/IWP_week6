0faf7c8bb0b0558e95c6b4e2edd06bf4ddf17ecb
KarvaMies <80526519+KarvaMies@users.noreply.github.com>
2022-10-11 16:27:34 +0300

    Task 2

--- src/index.js
+++ src/index.js
@@ -70,7 +70,39 @@ const getData = async () => {
 
 const buildChart = async () => {
   const data = await getData();
-  console.log(data);
+  //console.log(data);
+
+  const areas = Object.keys(data.dimension.Alue.category.label);
+  const labels = Object.values(data.dimension.Vuosi.category.label);
+  const values = data.value;
+
+  console.log(areas);
+  console.log(labels);
+  console.log(values);
+
+  areas.forEach((area, index) => {
+    let areaPopulation = [];
+    for (let i = 0; i < labels.length; i++) {
+      areaPopulation.push(values[i * areas.length + index]);
+    }
+    areas[index] = {
+      name: area,
+      values: areaPopulation
+    };
+  });
+
+  const chartData = {
+    labels: labels,
+    datasets: areas
+  };
+
+  const chart = new frappe.Chart("#chart", {
+    title: "Population growth of Finland",
+    data: chartData,
+    type: "line",
+    height: 450,
+    colors: ["#eb5146"]
+  });
 };
 
 buildChart();
