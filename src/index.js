c71270c9328331cce2cdc7347941b015e82a6143
KarvaMies <80526519+KarvaMies@users.noreply.github.com>
2022-10-11 14:06:59 +0300

    Task 1

--- src/index.js
+++ src/index.js
@@ -57,6 +57,13 @@ const getData = async () => {
     "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px";
 
   const res = await fetch(url, {
-    method: "POST"
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(jsonQuery)
   });
+  if (!res.ok) {
+    return;
+  }
+  const data = await res.json();
+  return data;
 };
